519fe5ae50c1afc83e88986ed0b177a1f30c5de9
juwonboy <juwonboy@gmail.com>
2026-04-17 12:36:52 +0900

    04_17

--- CProject05/ex05_13.c
+++ CProject05/ex05_13.c
@@ -2,8 +2,11 @@
 
 int main() {
 
-	char str[80];
+	char str[] = {1,2};
 	int a;
+	int len;
+	char szData[] = "Test String!";
+	char arr[8] = {1,2,4,6,3};
 
 	a = 200;
 
@@ -14,12 +17,20 @@ int main() {
 	printf("str:%p\n", str);
 	printf("&str:%p\n", &str);
 
-	char szData[] = "Test String!";
-
 	printf("%s\n", szData);
 	puts(szData);
 
 	printf("%c %c\n", szData[0], szData[11]);
+
+	printf("sizeof(arr) :%zd\n",sizeof(arr));
+	printf("sizeof(szData):%zd\n", sizeof(szData));
+
+	len = sizeof(arr) / sizeof(arr[0]);
+
+	for ( a = 0; a < len; a++)
+	{
+		printf(" %d :: %c\n", a, arr[a]);
+	}
 
 	return 0;
 }
---
+++ CProject05/ex05_14.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+
+	char str[] = "Good morring!";
+	int a;
+	int len;
+
+	len = sizeof(str) / sizeof(str[0]);
+
+	printf("%s\n",str);
+	printf("배열 str의 크기 :%zd\n", sizeof(str));
+	printf("null 문자 출력(문자): %c\n", str[len-1]);
+	printf("null 문자 출력(숫자): %d\n", str[len-1]);
+
+	str[12] = '?';
+
+	printf("문자출력 :: %c\n", str[12]);
+	printf("문자열 출력 :: %s\n", str);
+
+	for (a = 0; a < len; a++)
+	{
+		printf(" %d :: %c\n", a, str[a]);
+	}
+
+	return 0;
+}

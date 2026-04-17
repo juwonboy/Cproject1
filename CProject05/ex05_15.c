8c4f31ef062e6e69b5ac58fb97795953ebb01260
juwonboy <juwonboy@gmail.com>
2026-04-17 13:44:58 +0900

    1122

--- CProject05/ex05_15.c
+++ CProject05/ex05_15.c
@@ -2,27 +2,33 @@
 
 int main() {
 
-	char str[] = "There is no royal road to learning C.0";
+	char str1[] = "There is no royal road to learning C.";
 	char str2[80];
 	int a;
 	int len;
 
-	len = sizeof(str) / sizeof(str[0]);
+	len = sizeof(str1) / sizeof(str1[0]);
 
-	printf("%s\n", str);
-	printf("배열 str의 크기 :%zd\n", sizeof(str));
-	printf("null 문자 출력(문자): %c\n", str[len - 1]);
-	printf("null 문자 출력(숫자): %d\n", str[len - 1]);
+	printf("%s\n", str1);
+	printf("배열 str의 크기 :%zd\n", sizeof(str1));
+	printf("null 문자 출력(문자): %c\n", str1[len - 1]);
+	printf("null 문자 출력(숫자): %d\n", str1[len - 1]);
 
-	str[12] = '?';
+	//str[12] = '?';
 
-	printf("문자출력 :: %c\n", str[12]);
-	printf("문자열 출력 :: %s\n", str);
+	//printf("문자출력 :: %c\n", str[12]);
+	//printf("문자열 출력 :: %s\n", str);
 
 	for (a = 0; a < len; a++)
 	{
-		printf(" %d :: %c\n", a, str[a]);
+		str2[a] = str1[a];
+		//printf(" %d :: %c\n", a, str[a]);
 	}
 
+	printf("목사된 str2 :: %s\n", str2);
+	for (a = 0; a < len; a++)
+	{	
+		printf(" %d :: %c\n", a, str2[a]);
+	}	
 	return 0;
 }

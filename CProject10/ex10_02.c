d9efac2d0eacbc62fc26dbbf179af1eadb36884a
juwonboy <juwonboy@gmail.com>
2026-04-23 15:00:22 +0900

    2026_04_23 5

---
+++ CProject10/ex10_02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*
+    이중포인터를 이용한 문자열 교환
+*/
+void exchange(char**, char**);
+
+int main()
+{
+
+    char* ap[] = "success";
+    char* bp[] = "failure";
+
+    return 0;
+}
+
+void exchange(char** a, char** b)
+{
+}
